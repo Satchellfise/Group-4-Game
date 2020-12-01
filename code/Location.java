d4312a8c2b87b3490c0b25d4e3a8795a41f69b54
dacohen1 <54648478+dacohen1@users.noreply.github.com>
2020-12-01 08:49:36 -0500

    Update Location.java
    Updated some stuff

--- code/Location.java
+++ code/Location.java
@@ -10,10 +10,12 @@ import java.util.ArrayList;
 /**
  *
  * @author danielcohen
+ * This entire class is just a format, the main will need to instantiate
+ * each location, giving it neighbors and NPCs. Probably good to create a 
+ * factory class that constructs them and passes them back to main, to save
+ * space in the main class.
  */
 
-//This entire class is just a format, the main will need to instantiate
-// each location, giving it neighbors and NPCs
 public class Location {
    
     ArrayList<String>  _neighbors = new ArrayList<>(); // list of neighbors :)
@@ -22,15 +24,36 @@ public class Location {
     
     ArrayList<String> _objects = new ArrayList<>();
     
-    public Location(ArrayList<String> neighbors, ArrayList<String> npcs, ArrayList<String> objects) {
+    /*
+    Storing all this data as Strings and then decoding them is a bad hack,
+    I know. Will hopefully change when we fully flesh out these classes.
+    I don't know how else to store the list of adjacent locations, though.
+    */
+    
+    final String _name;
+    
+    public Location(ArrayList<String> neighbors, ArrayList<String> npcs, ArrayList<String> objects, String name) {
         _neighbors = neighbors;
         _npcs = npcs;
         _objects = objects;
+        _name = name;
     }
     
     public void printNeighbors() {
         for (int i = 0; i <_neighbors.size(); i++ ) {
             System.out.println(_neighbors.get(i));
+        }
+    }
+    
+    public void printNpcs() {
+        for (int i = 0; i <_npcs.size(); i++ ) {
+            System.out.println(_npcs.get(i));
+        }
+    }
+    
+     public void printObjects() {
+        for (int i = 0; i <_objects.size(); i++ ) {
+            System.out.println(_objects.get(i));
         }
     }
     
